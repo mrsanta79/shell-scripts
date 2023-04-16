65112e481b32b6857be539572a5ddb9029a8257f
Santanu Biswas <dev.sbiswas7991@gmail.com>
2023-04-16 22:26:14 +0530

    fix: messages fix for sh shell

--- laravel-deploy/deploy.sh
+++ laravel-deploy/deploy.sh
@@ -20,44 +20,44 @@ REDORANGE='\033[0;38m'
 NC='\033[0m' # No color
 
 # Show welcome message
-echo -e "\n${REDORANGE}This script will install and configure a LAMP stack for Laravel on your Ubuntu server."
-echo -e "${YELLOW}It is recommended to run this script on a fresh Ubuntu server installation."
-echo -e "\n${YELLOW}Before running this script, make sure you have superuser (sudo) privileges."
-echo -e "\n${GREEN}This script will install the following:"
-echo -e "${GREEN} - Apache web server"
-echo -e "${GREEN} - MySQL database server (MariaDB)"
-echo -e "${GREEN} - PHP"
-echo -e "${GREEN} - Composer"
-echo -e "${GREEN} - Laravel application from git repository"
-echo -e "${GREEN} - NodeJS"
-echo -e "${GREEN} - NPM"
-echo -e "${GREEN} - Yarn\n\n"
+echo "\n${YELLOW}This script will install and configure a LAMP stack for Laravel application on your Ubuntu server. It is recommended to run this script on a fresh Ubuntu server installation."
+echo "${YELLOW}Before running this script, make sure you have superuser (sudo) privileges."
+echo "\n${GREEN}This script will install the following:"
+echo "${GREEN} - Apache web server"
+echo "${GREEN} - MariaDB database server (Optional)"
+echo "${GREEN} - PHP"
+echo "${GREEN} - Composer"
+echo "${GREEN} - Laravel application from git repository"
+echo "${GREEN} - NodeJS"
+echo "${GREEN} - NPM"
+echo "${GREEN} - Yarn"
+echo "${GREEN} - Python 3\n\n"
 
 # Let user select php version or set default
-read -p "Choose your preferred PHP version (example: 7.4, 8.0, 8.1, 8.2; default: 8.2): " php_version
+echo -n "${LGRAY}Choose your preferred PHP version (example: 7.4, 8.0, 8.1, 8.2; default: 8.2): " && read php_version
 php_version=${php_version:-8.2}
 
 # Let user select node version or set default
-read -p "Choose your preferred NodeJS version (example: 14, 16, 18; default: 18): " node_version
+echo -n "\n${LGRAY}Choose your preferred NodeJS version (example: 14, 16, 18; default: 18): " && read node_version
 node_version=${node_version:-18}
 
 # Ask user if they want to install database or not
-read -p "Do you want to install MariaDB database? (y/n; default: n): " install_database
+echo -n "\n${LGRAY}Do you want to install MariaDB database? (y/n; default: n): " && read install_database
 install_database=${install_database:-n}
 
 # Ask for git url for laravel project
-read -p "Provide the git url of the laravel application: " git_url
+echo -n "\n${LGRAY}Provide the git url of the laravel application: " && read git_url
 
 # Ask for laravel project directory or set default
-read -p "Enter the website directory name where the application will be deployed under /var/www/<dir> (default: application): " site_dir_name
+echo -n "\n${LGRAY}Enter the website directory name where the application will be deployed under /var/www/<dir> (default: application): " && read site_dir_name
 site_dir_name=${site_dir_name:-application}
 
 # Ask for website domain name
-read -p "Enter website domain name (example: example.com): " site_domain
+echo -n "\n${LGRAY}Enter website domain name (example: example.com): " && read site_domain
 site_domain=${site_domain:-example.com}
 
 # Start installation
-echo -e "\n${PURPLE}Starting installation...\n"
+echo "\n${PURPLE}Starting installation...\n"
 
 # Update Ubuntu's package index
 sudo apt update
@@ -75,22 +75,22 @@ if [ "$install_database" = "y" ]; then
     sudo mysql_secure_installation
 
     # Guide step by step to create a new database for Laravel
-    echo -e "\n${GREEN}Please follow the steps below to create a new database for Laravel application when the MySQL prompt appears:"
+    echo "\n${GREEN}Please follow the steps below to create a new database for Laravel application when the MySQL prompt appears:"
 
-    echo -e "\n${CYAN}1. CREATE DATABASE \`[database-name]\`;"
-    echo -e "${CYAN}2. CREATE USER '[database-user]'@'localhost' IDENTIFIED BY '[database-password]';"
-    echo -e "${CYAN}3. GRANT ALL PRIVILEGES ON \`[database-name]\`.* TO '[database-user]'@'localhost' IDENTIFIED BY '[database-password]';"
-    echo -e "${CYAN}4. FLUSH PRIVILEGES;"
-    echo -e "${CYAN}5. EXIT;"
+    echo "\n${CYAN}1. CREATE DATABASE \`[database-name]\`;"
+    echo "${CYAN}2. CREATE USER '[database-user]'@'localhost' IDENTIFIED BY '[database-password]';"
+    echo "${CYAN}3. GRANT ALL PRIVILEGES ON \`[database-name]\`.* TO '[database-user]'@'localhost' IDENTIFIED BY '[database-password]';"
+    echo "${CYAN}4. FLUSH PRIVILEGES;"
+    echo "${CYAN}5. EXIT;"
 
-    echo -e "\n${REDORANGE}NOTE: Replace [database-name], [database-user] and [database-password] with your own values."
+    echo "\n${REDORANGE}NOTE: Replace [database-name], [database-user] and [database-password] with your own values."
 
     # Start MySQL prompt
-    echo -e "\n${GREEN}Starting MySQL prompt..."
+    echo "\n${GREEN}Starting MySQL prompt..."
     sudo mysql -u root -p
 
     # Continue installation
-    echo -e "\n${PURPLE}Continuing installation...\n"
+    echo "\n${PURPLE}Continuing installation...\n"
 fi
 
 # Install PHP version and necessary extensions
@@ -142,7 +142,7 @@ fi
 npm install -g npm yarn
 
 # Show logout and login warning for nvm
-echo -e "\n${YELLOW}Please logout and login again to use nvm manually.\n"
+echo "\n${YELLOW}Please logout and login again to use nvm manually.\n"
 
 # Configure a virtual host for Laravel
 sudo mkdir -p /var/www/$site_dir_name
@@ -210,9 +210,9 @@ cp .env.example .env
 php artisan key:generate
 
 # Finish message
-echo -e "\n\n${BLUE}Laravel application is now deployed and configured.\n"
-echo -e "\n${BLUE}To finalize the installation, please follow the steps below:\n"
-echo -e "${BLUE}1. Edit .env file and make necessry changes."
-echo -e "${BLUE}2. Run 'php artisan migrate:fresh --seed' to migrate database."
+echo "\n\n${BLUE}Laravel application is now deployed and configured.\n"
+echo "\n${BLUE}To finalize the installation, please follow the steps below:\n"
+echo "${BLUE}1. Edit .env file and make necessry changes."
+echo "${BLUE}2. Run 'php artisan migrate:fresh --seed' to migrate database."
 
 # End of script
